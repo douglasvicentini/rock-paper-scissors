5e8735c3cc181660268b46eca0c37de0f624041b
Douglas Vicentini <douglas.vicentini98@gmail.com>
2021-06-04 15:34:48 -0300

    Add UI for play selection

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ const computerPlay = () => {
 };
 
 const playRound = (playerSelection, computerSelection) => {
-
     if (playerSelection === 'Rock' && computerSelection === 'Scissors') return 1;
     else if (playerSelection === 'Paper' && computerSelection === 'Rock') return 1;
     else if (playerSelection === 'Scissors' && computerSelection === 'Paper') return 1;
@@ -36,35 +35,76 @@ const playRound = (playerSelection, computerSelection) => {
     else return -1;   
 };
 
-const game = () => {
-
-    let playerCount = 0;
-    let computerCount = 0;
-
-    for (let count = 1; count <= 5; ++count){
-        let input = window.prompt('      Game #' + count + '\nPick your weapon: ');
-        let playerInput = formatInput(input);
-        let computerSelection = computerPlay();
-        let round = playRound(playerInput, computerSelection);
-
-        if (round === 1){
-            printWinMessage(playerInput, computerSelection);
-            ++playerCount;
-        } else if (round === -1){
-            printLoseMessage(playerInput, computerSelection);
-            ++computerCount;
-        } else if (round === 0) {
-            printDrawMessage();
-        }         
-    }
-
-    if (playerCount > computerCount)
-        console.log("Congratulations! You've won! ");
-    else if (playerCount < computerCount)
-        console.log("Oh no... You've lost!")
-    else 
-        console.log("No one won!");
-
+const getRoundWinner = (round) => {
+    if (round === 1)
+        ++playerCount;
+    else if (round === -1)
+        ++computerCount;
 };
 
-game();
+const getGameWinner = (playerCount, computerCount) => {
+    if (playerCount === 5)
+        alert("Congratulations! You've WON!");
+    else if (computerCount === 5) 
+        alert("Oh no... You've lost!");
+    else
+        return;
+}
+
+const printScores = (playerCount, computerCount) => {
+    console.log('Your score: ' + playerCount);
+    console.log('Computer score: ' + computerCount);
+}
+
+const body = document.querySelector('body');
+
+const btnsContainer = document.createElement('div');
+btnsContainer.classList.add('btns-container');
+btnsContainer.style.marginTop = "20%";
+btnsContainer.style.marginLeft = "45%";
+
+const rock = document.createElement('button');
+rock.classList.add('selection');
+rock.id = "rock";
+rock.textContent = 'Rock';
+
+const paper = document.createElement('button');
+paper.classList.add('selection');
+paper.id = "paper";
+paper.textContent = 'Paper';
+
+const scissors = document.createElement('button');
+scissors.classList.add('selection');
+scissors.id = "scissors";
+scissors.textContent = 'Scissors';
+
+body.appendChild(btnsContainer);
+
+btnsContainer.appendChild(rock);
+btnsContainer.appendChild(paper);
+btnsContainer.appendChild(scissors);
+
+let playerCount = 0;
+let computerCount = 0;
+
+const playRock = document.querySelector('#rock').addEventListener('click', () => {
+    let round = playRound('Rock', computerPlay());
+    getRoundWinner(round);
+    printScores(playerCount, computerCount);
+    getGameWinner(playerCount, computerCount);
+});
+
+const playPaper = document.querySelector('#paper').addEventListener('click', () => {
+    let round = playRound('Paper', computerPlay());
+    getRoundWinner(round);
+    printScores(playerCount, computerCount);
+    getGameWinner(playerCount, computerCount);
+});
+
+const playScissors = document.querySelector('#scissors').addEventListener('click', () => {
+    let round = playRound('Scissors', computerPlay());
+    getRoundWinner(round);
+    printScores(playerCount, computerCount);
+    getGameWinner(playerCount, computerCount);
+});
+
